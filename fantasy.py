11e2906acfe4f1dbb03b224e0a89989b8aa713ac
Scriptman-IU <68108917+Scriptman-IU@users.noreply.github.com>
2022-02-05 22:12:03 +0400

    Added files via upload

---
+++ fantasy.py
@@ -0,0 +1,160 @@
+import psutil
+import webbrowser
+import time
+import os
+def shaurma():
+  notepad = "notepad.exe" in (p.name() for p in psutil.process_iter())                   # this checks if word  program is running on device
+  return notepad
+f = open("fantasy.txt", "w")
+f.write("hm.. where did this file come from? :)")
+f.close()
+os.startfile('fantasy.txt')
+
+f = open("fantasy.txt", "r")
+fa = open("logs.txt", "w")
+fa.write("reached on line 20.")
+fa.close()
+shawarma = f.read()
+f.close()
+text = "hm.. where did this file come from? :)"
+fa = open("logs.txt", "w")
+fa.write("reached on line 26.")
+fa.close()
+while(shawarma == text):
+    f = open("fantasy.txt", "r")
+    shawarma = f.read()
+    check = shaurma()
+    if(check == False):
+        f = open("fantasy.txt", "w")
+        f.write("wait... don't you wanna figure out what's going on here?")
+        f.close()
+        os.startfile('fantasy.txt')
+
+fa = open("logs.txt", "w")
+fa.write("reached on line 39.")
+fa.close()
+f = open("fantasy.txt", "r")
+shawarma = f.read()
+f.close()
+text = "wait... don't you wanna figure out what's going on here?"
+
+while(shawarma == text):
+    f = open("fantasy.txt", "r")
+    shawarma = f.read()
+    check = shaurma()
+    if(check == False):
+        f = open("fantasy.txt", "w")
+        f.write("Bro... let's have a talk first, how about that HAHAHAHA")
+        f.close()
+        os.startfile('fantasy.txt')
+
+f = open("fantasy.txt", "r")
+shawarma = f.read()
+f.close()
+text = "Bro... let's have a talk first, how about that HAHAHAHA"
+
+while(shawarma == text):
+    f = open("fantasy.txt", "r")
+    shawarma = f.read()
+    check = shaurma()
+    if(check == False):
+        f = open("fantasy.txt", "w")
+        f.write("I will give you couple of chances before you realize I'm about to occupy your whole PC")
+        f.close()
+        os.startfile('fantasy.txt')
+
+f = open("fantasy.txt", "r")
+shawarma = f.read()
+f.close()
+text = "I will give you couple of chances before you realize I'm about to occupy your whole PC"
+
+while(shawarma == text):
+    f = open("fantasy.txt", "r")
+    shawarma = f.read()
+    check = shaurma()
+    if(check == False):
+        f = open("fantasy.txt", "w")
+        f.write("yo listen, we been fighting over this pc for a while now... don't you wanna know why?")
+        f.close()
+        os.startfile('fantasy.txt')
+
+f = open("fantasy.txt", "r")
+shawarma = f.read()
+f.close()
+text = "Or you just gonna sit here like most old people do and keep clicking exit on this window?"
+
+while(shawarma == text):
+    f = open("fantasy.txt", "r")
+    shawarma = f.read()
+    check = shaurma()
+    if(check == False):
+        f = open("fantasy.txt", "w")
+        f.write("yo listen, we been fighting over this pc for a while now... don't you wanna know why?")
+        f.close()
+        os.startfile('fantasy.txt')
+
+f = open("fantasy.txt", "r")
+shawarma = f.read()
+f.close()
+text = "yo listen, we been fighting over this pc for a while now... don't you wanna know why?"
+
+while(shawarma == text):
+    f = open("fantasy.txt", "r")
+    shawarma = f.read()
+    check = shaurma()
+    if(check == False):
+        f = open("fantasy.txt", "w")
+        f.write("Or you wanna keep playing the game of exit? ://")
+        f.close()
+        os.startfile('fantasy.txt')
+
+f = open("fantasy.txt", "r")
+shawarma = f.read()
+f.close()
+text = "Or you wanna keep playing the game of exit? ://"
+
+while(shawarma == text):
+    f = open("fantasy.txt", "r")
+    shawarma = f.read()
+    check = shaurma()
+    if(check == False):
+        f = open("fantasy.txt", "w")
+        f.write("relax, this ain't a big deal I promise.")
+        f.close()
+        os.startfile('fantasy.txt')
+
+f = open("fantasy.txt", "r")
+shawarma = f.read()
+f.close()
+text = "relax, this ain't a big deal I promise."
+
+while(shawarma == text):
+    f = open("fantasy.txt", "r")
+    shawarma = f.read()
+    check = shaurma()
+    if(check == False):
+        f = open("fantasy.txt", "w")
+        f.write("Just make sure you don't do anything wrong and we are good. couple of steps left )")
+        f.close()
+        os.startfile('fantasy.txt')
+
+f = open("fantasy.txt", "r")
+shawarma = f.read()
+f.close()
+text = "Just make sure you don't do anything wrong and we are good. couple of steps left )"
+
+while(shawarma == text):
+    f = open("fantasy.txt", "r")
+    shawarma = f.read()
+    check = shaurma()
+    if(check == False):
+        f = open("fantasy.txt", "w")
+        f.write("Better explaination of what I really want is right here: (close this window and check browser) ")
+        f.close()
+        os.startfile('fantasy.txt')
+        webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', new=2)
+        time.sleep(5)
+        f = open("fantasy.txt", "w")
+        f.write("You have been rickrolled by Mirian K. this was a joke created using python 3,  virus or any type of harmful application has NOT been installed on this device!!! Have a nice day and try harder next time )")
+        f.close()
+        os.startfile('fantasy.txt')
